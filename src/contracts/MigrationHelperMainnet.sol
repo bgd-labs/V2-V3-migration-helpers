d76a5634f036496b52c424b8d7d3e7b428da4f65
Andrei Kozlov <kyzia.ru@gmail.com>
2023-01-22 10:53:57 +0200

    add allowance for wstETH

--- src/contracts/MigrationHelperMainnet.sol
+++ src/contracts/MigrationHelperMainnet.sol
@@ -10,25 +10,26 @@ import {MigrationHelper} from './MigrationHelper.sol';
 
 contract MigrationHelperMainnet is MigrationHelper {
   using SafeERC20 for IERC20WithPermit;
+  using SafeERC20 for IWstETH;
 
   IERC20WithPermit public constant STETH =
     IERC20WithPermit(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
   IWstETH public constant WSTETH =
     IWstETH(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0);
 
-  constructor(IPoolAddressesProvider v3AddressesProvider, IV2LendingPool v2Pool)
-    MigrationHelper(v3AddressesProvider, v2Pool)
-  {
+  constructor(
+    IPoolAddressesProvider v3AddressesProvider,
+    IV2LendingPool v2Pool
+  ) MigrationHelper(v3AddressesProvider, v2Pool) {
     STETH.safeApprove(address(WSTETH), type(uint256).max);
+    WSTETH.safeApprove(address(POOL), type(uint256).max);
   }
 
   //@Iinheritdoc MigrationHelper
-  function getMigrationSupply(address asset, uint256 amount)
-    external
-    view
-    override
-    returns (address, uint256)
-  {
+  function getMigrationSupply(
+    address asset,
+    uint256 amount
+  ) external view override returns (address, uint256) {
     if (asset == address(STETH)) {
       uint256 wrappedAmount = WSTETH.getWstETHByStETH(amount);
 
@@ -38,11 +39,10 @@ contract MigrationHelperMainnet is MigrationHelper {
     return (asset, amount);
   }
 
-  function _processSupply(address asset, uint256 amount)
-    internal
-    override
-    returns (address, uint256)
-  {
+  function _processSupply(
+    address asset,
+    uint256 amount
+  ) internal override returns (address, uint256) {
     if (asset == address(STETH)) {
       uint256 wrappedAmount = WSTETH.wrap(amount);
 
--- src/interfaces/IWstETH.sol
+++ src/interfaces/IWstETH.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-interface IWstETH {
+import {IERC20WithPermit} from 'solidity-utils/contracts/oz-common/interfaces/IERC20WithPermit.sol';
+
+interface IWstETH is IERC20WithPermit {
   function wrap(uint256 stETHAmount) external returns (uint256);
 
-  function getWstETHByStETH(uint256 stETHAmount)
-    external
-    view
-    returns (uint256);
+  function getWstETHByStETH(
+    uint256 stETHAmount
+  ) external view returns (uint256);
 }
